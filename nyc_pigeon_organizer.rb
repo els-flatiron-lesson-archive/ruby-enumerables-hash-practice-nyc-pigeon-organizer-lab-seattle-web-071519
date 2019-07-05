d4addd048dec59148fc5b62a51b882bb42223f97
Helen Marshall <marshallhelenm@gmail.com>
2019-07-05 19:19:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  name_hash = {}
+  data.each do |attribute, values_hash|
+    values_hash.each do |value, names_array|
+      names_array.each do |name|
+        if !name_hash[name]
+          name_hash[name] = {}
+        end
+        if !name_hash[name][attribute]
+          name_hash[name][attribute] = []
+        end
+        name_hash[name][attribute].push value.to_s
+ 
+        
+        end
+      end
+    end
+  name_hash
 end
