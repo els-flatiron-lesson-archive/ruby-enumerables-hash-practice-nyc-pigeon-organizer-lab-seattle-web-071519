9a7fa6bc75b15f5765d9a1cf1e0af239bfd9a71c
Helen Marshall <marshallhelenm@gmail.com>
2019-07-05 19:04:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   name_hash = {}
-  data.keys.each do |attribute|
-    data[attribute].keys.each do |key|
+  data.each do |key, value|
+    value.each do |key|
       data[attribute][key].each do |name|
         if name_hash[name]
           name_hash[name][:attribute] = key
